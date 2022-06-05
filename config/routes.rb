70c69aec6f30f91cd99b397b7a789d1bf9dc4736
kotayoshizawa <kot2@outlook.jp>
2022-06-05 11:31:23 +0900

    user_crete_and_login

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "toppage#index"
+  # ユーザ登録用のパス
+  get "signup", to: "users#new"
+  resources :users, only: [:show, :create]
+  # ログイン用のパス
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+    # ログアウト用
+  delete "logout", to: "sessions#destroy"
 end
