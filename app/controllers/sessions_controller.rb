7fb6801612772077773c6eb30b27a5bef793d5c4
kotayoshizawa <kot2@outlook.jp>
2022-06-05 17:14:57 +0900

    login_update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,34 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email,password)
+     flash[:success] = "ログイン成功"
+     redirect_to @user
+    else
+     flash.now[:danger] = "ログイン失敗" 
+     render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "ログアウト完了"
+    redirect_to root_url
+  end
+
+  private
+
+  def login(email,password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      # 成功時
+      session[:user_id] = @user.id
+      return true
+    else
+      # 失敗時
+      return false
+    end
   end
 end
